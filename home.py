779cf56ecd2815d1274b5f0fd88392ab12e26479
rgkalden <rgkalden@gmail.com>
2022-08-09 14:14:42 -0600

    Update app

--- home.py
+++ home.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("RecipePal")
 
 @st.cache()
 def loadRawData(filename, nrows):
-    return pd.read_csv(filename, nrows=nrows)
+    return pd.read_parquet(filename)[:nrows]
 
 
 def selectColumns(dataframe, columns):
@@ -18,20 +19,35 @@ def removeNullValues(dataframe):
     dataframe.dropna(axis=0, inplace=True)
 
 
-data = loadRawData('recipes.csv', 10000)
+data = loadRawData('recipes.parquet', 100)
 
 keepColumns = ['Name',
-               #'CookTime', 'PrepTime', 'TotalTime', 
-               'RecipeCategory', 'RecipeIngredientQuantities', 'RecipeIngredientParts',
-               'RecipeServings', 'RecipeInstructions']
+               'CookTime', 'PrepTime', 'TotalTime', 
+               'RecipeCategory', 
+               #'RecipeIngredientQuantities', 
+               'RecipeIngredientParts',
+               'RecipeInstructions']
 reducedData = selectColumns(data, keepColumns)
 
-removeNullValues(reducedData)
+removeNullValues(reducedData.drop('CookTime', axis=1))
 
 
 st.dataframe(reducedData)
 
 
+figCategories = plt.figure(figsize=(12, 4))
+plt.hist(reducedData['RecipeCategory'], bins=30)
+plt.title('Recipe Categories')
+plt.ylabel('Count')
+plt.xticks(rotation=90)
+st.pyplot(figCategories)
+
+figTotalTime = plt.figure(figsize=(12, 4))
+plt.hist(reducedData['TotalTime'], bins=30)
+plt.title('Cooking Times')
+plt.ylabel('Count')
+plt.xticks(rotation=90)
+st.pyplot(figTotalTime)
 
 
 
