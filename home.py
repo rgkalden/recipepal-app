9b0927ccc37e9c1fc8f27d1b9ffefb660b93018b
rgkalden <rgkalden@gmail.com>
2022-08-11 20:00:15 -0600

    Add selectors for filter

--- home.py
+++ home.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Functions
+
+
 @st.cache()
 def loadRawData(filename, nrows):
     return pd.read_parquet(filename)[:nrows]
@@ -19,8 +21,10 @@ def convertTimes(dataframe, columns):
         dataframe[column] = pd.to_timedelta(
             dataframe[column], errors='coerce') / np.timedelta64(1, 'm')
 
+
 def removeNullValues(dataframe):
     dataframe.dropna(axis=0, inplace=True)
+
 
 def searchItem(dataframe, column, target):
     series = dataframe[column].to_list()
@@ -33,7 +37,13 @@ def searchItem(dataframe, column, target):
 
     return foundIndex
 
+
+def to_1D(series):
+    return pd.Series([x for _list in series for x in _list])
+
+
 # Title and How To
+
 
 st.title("RecipePal 🍲")
 
@@ -50,6 +60,7 @@ with st.expander("How to use this app"):
      """)
 
 # Load and Process data
+
 
 data = loadRawData('recipes-sampled.parquet', 100)
 
@@ -68,39 +79,53 @@ removeNullValues(reducedData)
 
 # Display Data
 
-recipeCategories = reducedData['RecipeCategory'].unique().tolist()
-selection = st.multiselect('Choose category', recipeCategories)
-filteredData = reducedData[reducedData['RecipeCategory'].isin(selection)]
+filterOnCategory = st.checkbox('Filter on Category')
 
-# max time range arbitrarily set to 120
-totalTimeRange = st.slider('Select a range of Total Cooking Time (minutes)', 0, 120, (25, 75))
-filteredData = filteredData[(filteredData['TotalTime'] >= totalTimeRange[0]) & (filteredData['TotalTime'] <= totalTimeRange[1])]
+if filterOnCategory:
+    recipeCategories = reducedData['RecipeCategory'].unique().tolist()
+    selection = st.multiselect('Choose category', recipeCategories)
+    filteredData = reducedData[reducedData['RecipeCategory'].isin(selection)]
 
-ingredient = st.text_input('Ingredient Name', '')
 
-foundIndex = searchItem(filteredData, column='RecipeIngredientParts', target=ingredient)
-if ingredient != '':
-    filteredData = filteredData.iloc[foundIndex]
+filterOnTime = st.checkbox('Filter on cooking time')
 
-st.dataframe(filteredData)
+if filterOnTime:    
+    maxCookingTime = int(reducedData['TotalTime'].max())
+    totalTimeRange = st.slider('Select a range of Total Cooking Time (minutes)', 0, maxCookingTime, (0, 120))
+    filteredData = filteredData[(filteredData['TotalTime'] >= totalTimeRange[0]) & (filteredData['TotalTime'] <= totalTimeRange[1])]
 
-if len(filteredData) == 0:
-    st.write('No recipes found')
-else:
-    st.write(len(filteredData), " recipes found")
+filterOnIngredient = st.checkbox('Filter by Ingredient')
+
+if filterOnIngredient:
+    ingredient = st.text_input('Ingredient Name', '')
+
+    foundIndex = searchItem(filteredData, column='RecipeIngredientParts', target=ingredient)
+    if ingredient != '':
+        filteredData = filteredData.iloc[foundIndex]
+
+if filterOnCategory or filterOnTime or filterOnIngredient:
+    st.dataframe(filteredData)
+
+    if len(filteredData) == 0:
+        st.write('No recipes found')
+    else:
+        st.write(len(filteredData), " recipes found")
+
 
 # Visualize recipe database
+
 
 st.subheader('Not sure what you want to cook? 😕')
 st.write('Visualization of your recipe database')
 
+
 # Chart for recipe categories
+
 
 topNumberCategories = st.slider('Number of categories to display?', 0, 20, 10)
 st.write('Top ', topNumberCategories, 'categories are displayed')
 topCategories = reducedData['RecipeCategory'].value_counts().index.to_list()[:topNumberCategories]
-topCategoriesDataframe = reducedData[reducedData['RecipeCategory'].isin(
-    topCategories)]
+topCategoriesDataframe = reducedData[reducedData['RecipeCategory'].isin(topCategories)]
 
 figCategories = plt.figure(figsize=(12, 4))
 plt.hist(topCategoriesDataframe['RecipeCategory'])
@@ -109,7 +134,9 @@ plt.xticks(rotation=90)
 plt.xlabel('Recipe Name')
 st.pyplot(figCategories)
 
+
 # Chart for cooking times
+
 
 st.write('Distribution of Cooking Times')
 figTotalTime = plt.figure(figsize=(12, 4))
@@ -119,14 +146,13 @@ plt.xticks(rotation=90)
 plt.xlabel('Total Cooking Time (minutes)')
 st.pyplot(figTotalTime)
 
+
 # Chart for ingredients
 
+
 st.write('Distribution of Ingredients')
-def to_1D(series):
-    return pd.Series([x for _list in series for x in _list])
 
 topNumberIngredients = 20
-
 labels = to_1D(topCategoriesDataframe['RecipeIngredientParts']).value_counts().index[:topNumberIngredients]
 values = to_1D(topCategoriesDataframe['RecipeIngredientParts']).value_counts().values[:topNumberIngredients]
 
